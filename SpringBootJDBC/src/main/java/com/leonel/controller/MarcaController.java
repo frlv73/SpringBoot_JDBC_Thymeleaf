63652f56bd70ec39bed0ac3bc64fe5d72f434402
frlv73 <leovilla91@gmail.com>
2018-01-12 12:32:58 -0300

    Templates, formulario web y add en controlador. Redirección a páginas fuera de la carpeta marca con IndexController.

--- SpringBootJDBC/src/main/java/com/leonel/controller/MarcaController.java
+++ SpringBootJDBC/src/main/java/com/leonel/controller/MarcaController.java
@@ -6,7 +6,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.leonel.model.Marca;
 import com.leonel.service.IMarcaService;
 
 @Controller
@@ -16,9 +18,12 @@ public class MarcaController {
 	@Autowired
 	private IMarcaService service;
 	
+	//Redirige a la lista de marcas
 	@RequestMapping(method = RequestMethod.GET)
 	public String lista(ModelMap modelMap) {
 		modelMap.put("listaMarcas", service.getAllMarcas());
+		modelMap.addAttribute("titulo", "Lista de Marcas");
+		
 		return "marca/lista";
 	}
 	
@@ -31,6 +36,30 @@ public class MarcaController {
 		
 		// Abre el archivo detalle
 		return "marca/detalle";
+	}
+	
+	//Redirige al formulario para agregar marcas
+	@RequestMapping(value="agregar", method=RequestMethod.GET)
+	public String marcaform(ModelMap modelMap) {
+		modelMap.put("titulo", "Agregar Marca");
+		return "marca/marcaForm";
+	}
+	
+	//Agrega la marca cargada en el formulario y redirige a la página con la lista de marcas
+	@RequestMapping(value="agregar", method=RequestMethod.POST)
+	public String add(@RequestParam("txtID") int id, @RequestParam("txtNombre") String nombre ,ModelMap modelMap) {
+		
+		//Crea una nueva instancia de marca y le asigna los valores recibidos del form
+		Marca m = new Marca();
+		m.setId(id);
+		m.setNombre(nombre);
+		
+		//Agrega la marca a la base de datos
+		service.addMarca(m);
+		
+		//Trae todas las marcas otra vez y redirige a la lista
+		modelMap.put("listaMarcas", service.getAllMarcas());
+		return "marca/lista";
 	}
 
 }
